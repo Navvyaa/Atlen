a176160094badc92bafa675e05c464eea6366dd6
Navya Gupta <navyagupta171@gmail.com>
2025-01-05 12:29:32 +0530

    Wrap RegisterForm in Suspense for improved loading state handling

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -1,4 +1,5 @@
 import { NextPage } from "next";
+import React, { Suspense } from 'react';
 
 import Navbar from "@/app/components/Navbar";
 import RegisterForm from "@/app/components/auth/RegisterForm";
@@ -10,8 +11,9 @@ const Register: NextPage = () => {
     <div className="w-full">
       <Navbar />
       {/* <EmailProvider> */}
-      <RegisterForm step={1}/>
-      {/* </EmailProvider> */}
+      <Suspense fallback={<div>Loading...</div>}>
+     <RegisterForm step={1}/>
+      </Suspense>
     </div>
   );
 };
